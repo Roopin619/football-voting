1ec914af118b33b46f49fc14e805f97ebc08f4ce
Roopin <bhadriroopin748@gmail.com>
2020-04-22 21:19:26 +0530

    Updates

---
+++ precache-manifest.c6ced66713b8cab928ae518b2feef424.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa3a1c54ae8ade89153a60266aeff56",
+    "url": "/football-voting/index.html"
+  },
+  {
+    "revision": "d8c0aa72437705c7207e",
+    "url": "/football-voting/static/css/main.f2edec28.chunk.css"
+  },
+  {
+    "revision": "421c561af57b70c49824",
+    "url": "/football-voting/static/js/2.8db35b86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/football-voting/static/js/2.8db35b86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c0aa72437705c7207e",
+    "url": "/football-voting/static/js/main.ef6d88d7.chunk.js"
+  },
+  {
+    "revision": "cceb3f68ebc3a819401f",
+    "url": "/football-voting/static/js/runtime-main.2ed2f2d9.js"
+  },
+  {
+    "revision": "124393ed40d3e9278fa61d704f7447d8",
+    "url": "/football-voting/static/media/barcelona.124393ed.png"
+  },
+  {
+    "revision": "f9ba305086b04af743dad54071abf31c",
+    "url": "/football-voting/static/media/bayern-munich.f9ba3050.png"
+  },
+  {
+    "revision": "f51c530a44a08bb367c43f32fbb4160f",
+    "url": "/football-voting/static/media/liverpool.f51c530a.png"
+  },
+  {
+    "revision": "e974351b992b8f465b5c97c93953a5db",
+    "url": "/football-voting/static/media/real-madrid.e974351b.png"
+  }
+]);
